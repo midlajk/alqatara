ae917d791e43c982200001187dbffaec40584321
midlaj <midlajk18@gmail.com>
2024-12-26 20:32:41 +0530

    basic screens

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/utilities', function(req, res, next) {
+  res.render('utilities/utilities', { title: 'Express' });
+});
+router.get('/utilities/addtruck', function(req, res, next) {
+  res.render('utilities/addtruck', { title: 'Express' });
+});
+
+router.get('/orders', function(req, res, next) {
+  res.render('orders/orders', { title: 'Express' });
+});
+router.get('/orders/neworder', function(req, res, next) {
+  res.render('orders/neworder', { title: 'Express' });
+});
+
+router.get('/customers', function(req, res, next) {
+  res.render('customers/customers', { title: 'Express' });
+});
+router.get('/customers/newcustomer', function(req, res, next) {
+  res.render('customers/newcustomer', { title: 'Express' });
+});
+
+
+router.get('/reports', function(req, res, next) {
+  res.render('customers/customers', { title: 'Express' });
+});
 module.exports = router;

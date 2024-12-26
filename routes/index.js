1e8297ed0fd08b02420ce26102f78074091e9394
midlaj <midlajk18@gmail.com>
2024-12-26 22:16:34 +0530

    basic screens

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',route:'dashboard' });
 });
 
 router.get('/utilities', function(req, res, next) {
-  res.render('utilities/utilities', { title: 'Express' });
+  res.render('utilities/utilities', { title: 'Express' ,route:'utilities'});
 });
 router.get('/utilities/addtruck', function(req, res, next) {
-  res.render('utilities/addtruck', { title: 'Express' });
+  res.render('utilities/addtruck', { title: 'Express',route:'dashboard' });
 });
 
 router.get('/orders', function(req, res, next) {
-  res.render('orders/orders', { title: 'Express' });
+  res.render('orders/orders', { title: 'Express',route:'orders' });
 });
 router.get('/orders/neworder', function(req, res, next) {
-  res.render('orders/neworder', { title: 'Express' });
+  res.render('orders/neworder', { title: 'Express',route:'orders' });
 });
 
 router.get('/customers', function(req, res, next) {
-  res.render('customers/customers', { title: 'Express' });
+  res.render('customers/customers', { title: 'Express',route:'customers' });
 });
 router.get('/customers/newcustomer', function(req, res, next) {
-  res.render('customers/newcustomer', { title: 'Express' });
+  res.render('customers/newcustomer', { title: 'Express',route:'customers' });
 });
 
 
 router.get('/reports', function(req, res, next) {
-  res.render('customers/customers', { title: 'Express' });
+  res.render('customers/customers', { title: 'Express' ,route:'reports'});
 });
 module.exports = router;
